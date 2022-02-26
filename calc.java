841ae6f1428aff582894439d36c930372cd1893b
SIDHARTH K S <91144938+SIDHARTHKS60@users.noreply.github.com>
2022-02-26 12:07:46 +0530

    Update calc.java

--- calc.java
+++ calc.java
@@ -4,7 +4,8 @@ public class calc {
 int a,b;
 a=20;b=30;
 int c=a+b;
-System.out.println("The sum of "+a+" & "+b+" is "+c)
+System.out.println("The sum of "+a+" & "+b+" is "+c);
+        System.out.println("3rd build also committed ");
 
     }
 }
